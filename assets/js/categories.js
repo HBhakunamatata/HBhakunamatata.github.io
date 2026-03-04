209bb19cfe19c934f3738605ab82783803e807d7
HBhakunamatata <hanbo9501@gmail.com>
2026-03-04 15:13:22 +0800

    feat(tag & category): add collapse for tag & category when page redirect with aside <a>

---
+++ assets/js/categories.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+    let hash = window.location.hash.substring(1).replace("-arch", "")
+    if (hash) {
+        console.log(hash)
+        showTarget(hash)
+    }
+
+    // let buttons = $("button[aria-controls]")
+    let asideCategories = $(".aside-category")
+    asideCategories.each(function (index, item) {
+        $(item).on("click", function (e) {
+            e.preventDefault()
+            console.log(item.textContent.trim())
+            showTarget(`${item.textContent.trim().toLowerCase()}`)
+
+            // collapse other categories
+            $(`button[aria-controls]`).not(`button[aria-controls=${item.textContent.trim().toLowerCase()}]`).each(function (index, item) {
+                item.classList.add("collapsed")
+                document.getElementById(item.getAttribute("aria-controls")).classList.remove("show")
+            })
+        });
+    });
+});
+
+function showTarget(target) {
+    let buttonElement = $(`button[aria-controls=${target}]`)[0]
+    let collapseElement = document.getElementById(target)
+    if (buttonElement && collapseElement) {
+        buttonElement.classList.remove("collapsed")
+        collapseElement.classList.add("show")
+    }
+}
+
+function collapseTarget(target) {
+    let buttonElement = $(`button[aria-controls=${target}]`)[0]
+    let collapseElement = document.getElementById(target)
+    if (buttonElement && collapseElement) {
+        buttonElement.classList.add("collapsed")
+        collapseElement.classList.remove("show")
+    }
+}
